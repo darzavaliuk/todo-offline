e098c161cdf46131160696e0d298e2253b5ed27c
Daria <dar.zavaliuk@gmail.com>
2023-07-20 17:19:56 +0300

    add redux, typescript

--- frontend/src/utils/constants.js
+++
@@ -1,6 +0,0 @@
-export const url = 'https://backend-flax-six.vercel.app/'
-export const TODOS = 'todos'
-export const QUEUE = 'queue'
-export const CREATE = 'create'
-export const DELETE = 'delete'
-export const UPDATE = 'update'
